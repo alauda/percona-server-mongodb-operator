7ed8e185764fde142f6842089503d3f1061e9302
Xiaolu Zhao <xlzhao@alauda.io>
2021-01-22 17:39:37 +0800

    reduce func's cyclometic complexity

--- pkg/controller/perconaservermongodb/monitor.go
+++ pkg/controller/perconaservermongodb/monitor.go
@@ -20,8 +20,15 @@ func init() {
 }
 
 func (r *ReconcilePerconaServerMongoDB) reconcileMonitor(enabled bool) error {
+	err := r.reconcileServiceMonitors(enabled)
+	if err != nil {
+		return err
+	}
+	return r.reconcileGrafanaDashboards(enabled)
+}
+
+func (r *ReconcilePerconaServerMongoDB) reconcileServiceMonitors(enabled bool) error {
 	sm := monitor.GenerateServiceMonitor()
-	gd := monitor.GenerateGrafana(WatchNamespace)
 	config, err := rest.InClusterConfig()
 	if err != nil {
 		log.Error(err, "error loading kubernetes configuration inside cluster")
@@ -29,48 +36,50 @@ func (r *ReconcilePerconaServerMongoDB) reconcileMonitor(enabled bool) error {
 	}
 	p, err := promClient.NewForConfig(config)
 	if err != nil {
-		log.Error(err, "error get prometheus client")
+		log.Error(err, "error create prometheus client")
 		return err
 	}
-
+	_, err = p.MonitoringV1().ServiceMonitors(WatchNamespace).Get(sm.Name, v1.GetOptions{})
+	if err != nil && !errors.IsNotFound(err) {
+		log.Error(err, "get servicemonitor fail")
+		return err
+	}
 	if enabled {
-		m, _ := p.MonitoringV1().ServiceMonitors(WatchNamespace).Get(sm.Name, v1.GetOptions{})
-		if m.Name == "" {
+		if errors.IsNotFound(err) {
 			_, err = p.MonitoringV1().ServiceMonitors(WatchNamespace).Create(sm)
 			if err != nil {
 				log.Error(err, "create servicemonitor fail")
-				return err
-			}
-		}
-
-		err = r.client.Get(context.TODO(),
-			types.NamespacedName{Name: gd.Name,
-				Namespace: gd.Namespace},
-			gd)
-		if err != nil {
-			if errors.IsNotFound(err) {
-				err = r.client.Create(context.TODO(), gd)
-				if err != nil {
-					log.Error(err, "Cannot create grafana dashboard")
-				}
-			} else {
-				log.Error(err, "get grafana dashboard fail")
 			}
 		}
 	} else {
-		m, _ := p.MonitoringV1().ServiceMonitors(WatchNamespace).Get(sm.Name, v1.GetOptions{})
-		if m.Name != "" {
-			err = p.MonitoringV1().ServiceMonitors(WatchNamespace).Delete(m.Name, &v1.DeleteOptions{})
+		if err == nil {
+			err = p.MonitoringV1().ServiceMonitors(WatchNamespace).Delete(sm.Name, &v1.DeleteOptions{})
 			if err != nil {
 				log.Error(err, "delete servicemonitor fail")
-				return err
 			}
 		}
+	}
+	return err
+}
 
-		err = r.client.Get(context.TODO(),
-			types.NamespacedName{Name: gd.Name,
-				Namespace: gd.Namespace},
-			gd)
+func (r *ReconcilePerconaServerMongoDB) reconcileGrafanaDashboards(enabled bool) error {
+	gd := monitor.GenerateGrafana(WatchNamespace)
+	err := r.client.Get(context.TODO(),
+		types.NamespacedName{Name: gd.Name,
+			Namespace: gd.Namespace},
+		gd)
+	if err != nil && !errors.IsNotFound(err) {
+		log.Error(err, "Get grafana dashboards fail")
+		return err
+	}
+	if enabled {
+		if errors.IsNotFound(err) {
+			err = r.client.Create(context.TODO(), gd)
+			if err != nil {
+				log.Error(err, "Cannot create grafana dashboard")
+			}
+		}
+	} else {
 		if err == nil {
 			err = r.client.Delete(context.TODO(), gd)
 			if err != nil {
@@ -79,10 +88,7 @@ func (r *ReconcilePerconaServerMongoDB) reconcileMonitor(enabled bool) error {
 		} else if errors.IsNotFound(err) {
 			// expected result achieved
 			err = nil
-		} else {
-			log.Error(err, "Get grafana dashboards fail")
 		}
 	}
-
 	return err
 }
