13c3507ebbf83284dfda97cff23d12ae79370b05
Xiaolu Zhao <xlzhao@alauda.io>
2021-02-18 16:27:26 +0800

    remove unnecessary log and pass not-found error

--- pkg/controller/perconaservermongodb/monitor.go
+++ pkg/controller/perconaservermongodb/monitor.go
@@ -33,37 +33,29 @@ func (r *ReconcilePerconaServerMongoDB) reconcileMonitor(enabled bool) error {
 func (r *ReconcilePerconaServerMongoDB) reconcileServiceMonitors(enabled bool) error {
 	owner, err := generateOwnerReference(r)
 	if err != nil {
-		log.Error(err, "Cannot generate OwnerReference")
 		return err
 	}
 	sm := monitor.GenerateServiceMonitor(owner)
 	config, err := rest.InClusterConfig()
 	if err != nil {
-		log.Error(err, "error loading kubernetes configuration inside cluster")
 		return err
 	}
 	p, err := promClient.NewForConfig(config)
 	if err != nil {
-		log.Error(err, "error create prometheus client")
 		return err
 	}
 	m, err := p.MonitoringV1().ServiceMonitors(WatchNamespace).Get(sm.Name, v1.GetOptions{})
 	if err != nil && !errors.IsNotFound(err) {
-		log.Error(err, "get servicemonitor fail")
 		return err
 	}
 	if enabled {
 		if errors.IsNotFound(err) {
 			_, err = p.MonitoringV1().ServiceMonitors(WatchNamespace).Create(sm)
-			if err != nil {
-				log.Error(err, "create servicemonitor fail")
-			}
 		} else if !reflect.DeepEqual(m.Spec, sm.Spec) {
 			_, err = p.MonitoringV1().ServiceMonitors(WatchNamespace).Update(sm)
-			if err != nil {
-				log.Error(err, "update servicemonitor fail")
-			}
 		}
+	} else {
+		err = nil
 	}
 	return err
 }
@@ -72,7 +64,6 @@ func (r *ReconcilePerconaServerMongoDB) reconcileGrafanaDashboards(enabled bool)
 	old_gd := &gdv1beta1.GrafanaDashboard{}
 	owner, err := generateOwnerReference(r)
 	if err != nil {
-		log.Error(err, "Generate owner reference fail")
 		return err
 	}
 	gd := monitor.GenerateGrafana(WatchNamespace, owner)
@@ -81,22 +72,17 @@ func (r *ReconcilePerconaServerMongoDB) reconcileGrafanaDashboards(enabled bool)
 			Namespace: gd.Namespace},
 		old_gd)
 	if err != nil && !errors.IsNotFound(err) {
-		log.Error(err, "Get grafana dashboards fail")
 		return err
 	}
 	if enabled {
 		if errors.IsNotFound(err) {
 			err = r.client.Create(context.TODO(), gd)
-			if err != nil {
-				log.Error(err, "Cannot create grafana dashboard")
-			}
 		} else if !reflect.DeepEqual(old_gd.Spec, gd.Spec) {
 			old_gd.Spec = gd.Spec
 			err = r.client.Update(context.TODO(), old_gd)
-			if err != nil {
-				log.Error(err, "Cannot update grafana dashboard")
-			}
 		}
+	} else {
+		err = nil
 	}
 	return err
 }
@@ -109,7 +95,6 @@ func generateOwnerReference(r *ReconcilePerconaServerMongoDB) (v1.OwnerReference
 			Namespace: WatchNamespace},
 		deploy)
 	if err != nil {
-		log.Error(err, "Cannot get Deployment")
 		return v1.OwnerReference{}, err
 	}
 	owner := v1.OwnerReference{
